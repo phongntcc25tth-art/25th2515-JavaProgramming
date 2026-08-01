48d3aeeb6cfe2cc4ce3e87759492366bad05328a
phongntcc25tth-art <phong.nt.cc25tth@ntu.edu.vn>
2026-08-01 23:09:17 +0700

    Baitap1_DTB

---
+++ HelloWorld1/src/HelloWorld.java
@@ -0,0 +1,14 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 System.out.println("Hello World!");
+		 System.out.println("Tôi tên là Nguyễn Tấn Phong.");
+	        System.out.println("Tôi là sinh viên.");
+	        System.out.println("Đây là chương trình Java đầu tiên của tôi.");
+	        System.out.println("Tôi đang học lập trình Java bằng Eclipse IDE.");
+	        System.out.println("tôi thêm một dòng mới , và sữa lại chữ java ở trên");
+	}
+
+}
