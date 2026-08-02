1cfd354605f940ea8cff38ada2671a57c27e4535
phongntcc25tth-art <phong.nt.cc25tth@ntu.edu.vn>
2026-08-03 01:55:25 +0700

    baitap3Bài tập thực hành
    Tính toán đơn giản

---
+++ baitap3_p1/src/baitap3_p1/BaiTap3.java
@@ -0,0 +1,28 @@
+package baitap3_p1;
+
+public class BaiTap3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Khai báo hai số nguyên
+        int a = 20;
+        int b = 5;
+
+        // In giá trị của a và b
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        // Thực hiện phép cộng
+        System.out.println("Cộng: " + (a + b));
+
+        // Thực hiện phép trừ
+        System.out.println("Trừ: " + (a - b));
+
+        // Thực hiện phép nhân
+        System.out.println("Nhân: " + (a * b));
+
+        // Thực hiện phép chia
+        System.out.println("Chia: " + (a / b));
+	}
+
+}
