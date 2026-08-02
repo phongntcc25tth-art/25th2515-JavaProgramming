50af8a5762ad08c9e0b1a79c1423fc5e2fd3df90
phongntcc25tth-art <phong.nt.cc25tth@ntu.edu.vn>
2026-08-02 10:29:21 +0700

    baitap0demo
    chu de Github Desktop + Demo

---
+++ Demohello/src/Hello.java
@@ -0,0 +1,13 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 System.out.println("Xin chào GitHub!");
+	        System.out.println("Đây là bài Demo Lab 0.");
+	        System.out.println("Đây là lần cập nhật thứ nhất.");
+	        System.out.println("Thêm file Hello.");
+	        System.out.println("Lần chỉnh sửa thứ nhất.");
+	}
+
+}
