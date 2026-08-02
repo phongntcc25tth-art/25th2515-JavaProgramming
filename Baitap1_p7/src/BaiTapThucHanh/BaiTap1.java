c4753edd3599d365a2f510b2ae45de9b76ab2a7b
phongntcc25tth-art <phong.nt.cc25tth@ntu.edu.vn>
2026-08-03 01:04:17 +0700

    Bài Tập1 Thực Hành
     Khai báo và tính toán

---
+++ Baitap1_p7/src/BaiTapThucHanh/BaiTap1.java
@@ -0,0 +1,23 @@
+package BaiTapThucHanh;
+
+public class BaiTap1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		   // 1. Khai báo các biến
+        int a = 15;
+        int b = 4;
+        double c = 3.5;
+        
+        // 2. Tính toán
+        int tong = a + b;
+        double thuong = a / c;
+        int phanDu = a % b;
+        
+        // 3. In kết quả
+        System.out.println("Tổng: " + tong);
+        System.out.println("Thương: " + thuong);
+        System.out.println("Phần dư: " + phanDu);
+	}
+
+}
