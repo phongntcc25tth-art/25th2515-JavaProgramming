058388490a6cb0c73512198931725aae09310c52
phongntcc25tth-art <phong.nt.cc25tth@ntu.edu.vn>
2026-08-03 02:18:19 +0700

    baitap4bBài tập thực hành
    Sử dụng var

---
+++ Bài tập 4b_p1Hello/src/baitap4b_p1/HelloVar.java
@@ -0,0 +1,16 @@
+package baitap4b_p1;
+import java.util.List;
+public class HelloVar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		var message = "Hello, World with var!";
+        var number = 42;
+        var list = List.of("Java", "Modern", "Features");
+
+        System.out.println(message);
+        System.out.println("Number: " + number);
+        System.out.println("List: " + list);
+	}
+
+}
