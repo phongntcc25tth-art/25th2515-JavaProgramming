44ab1aab9bbe81225e569cc33fdc0361e5ea6140
phongntcc25tth-art <phong.nt.cc25tth@ntu.edu.vn>
2026-08-03 01:52:19 +0700

    baitap2_p1Bài tập thực hành
    In hình ảnh đơn giản

---
+++ baitap2_p1/src/baitap2_p1/baitap2_HinhVe.java
@@ -0,0 +1,15 @@
+package baitap2_p1;
+
+public class baitap2_HinhVe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// In hình tam giác bằng dấu *
+        System.out.println("   *   ");
+        System.out.println("  ***  ");
+        System.out.println(" ***** ");
+        System.out.println("*******");
+
+	}
+
+}
