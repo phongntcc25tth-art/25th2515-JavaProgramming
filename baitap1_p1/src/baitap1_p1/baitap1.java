40d7bf8ac958be47580e0beebc2f536abeb2b4df
phongntcc25tth-art <phong.nt.cc25tth@ntu.edu.vn>
2026-08-03 01:33:43 +0700

    baitap1Bài tập thực hànhlab
    Thay đổi thông điệp

---
+++ baitap1_p1/src/baitap1_p1/baitap1.java
@@ -0,0 +1,16 @@
+package baitap1_p1;
+
+public class baitap1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		        System.out.println("Xin chào, tôi đang học Java!");
+		        System.out.println("CHÀO MỪNG ĐẾN VỚI JAVA");
+		        System.out.println("Họ tên: Nguyễn Tấn Phong");
+		        System.out.println("Lớp: ............");
+		        System.out.println("Trường: ............");
+		        System.out.println("Môn học: Lập trình Java");
+		    }
+	}
+
