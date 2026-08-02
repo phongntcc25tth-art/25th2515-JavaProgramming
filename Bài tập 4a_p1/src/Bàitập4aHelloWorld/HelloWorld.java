b9c9fc2e11df497a3780b5e2091c311d9f22160d
phongntcc25tth-art <phong.nt.cc25tth@ntu.edu.vn>
2026-08-03 02:06:05 +0700

    Bàitập4aBài tập thực hành
    HelloWorld (Java 17+)

---
+++ Bài tập 4a_p1/src/Bàitập4aHelloWorld/HelloWorld.java
@@ -0,0 +1,20 @@
+package Bàitập4aHelloWorld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 // Sử dụng Text Blocks (Java 17+)
+        String greeting = """
+                ======================
+                HELLO, MODERN JAVA!
+                ======================
+                by Nguyễn Tấn Phong
+                ======================
+                """;
+
+        // Hiển thị nội dung ra màn hình
+        System.out.println(greeting);
+	}
+
+}
