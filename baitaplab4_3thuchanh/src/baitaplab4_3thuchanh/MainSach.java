0993739b9ca71cbaf47d36bbe2da457e14e16813
phongntcc25tth-art <phong.nt.cc25tth@ntu.edu.vn>
2026-08-06 17:30:08 +0700

    Update MainSach.java

--- baitaplab4_3thuchanh/src/baitaplab4_3thuchanh/MainSach.java
+++ baitaplab4_3thuchanh/src/baitaplab4_3thuchanh/MainSach.java
@@ -1,5 +1,28 @@
 package baitaplab4_3thuchanh;
 
 public class MainSach {
+	 public static void main(String[] args) {
 
+	        Sach s1 = new Sach();
+	        s1.tieuDe = "Lập trình Java";
+	        s1.tacGia = "Nguyễn Văn A";
+	        s1.gia = 120000;
+	        s1.soTrang = 300;
+
+	        Sach s2 = new Sach();
+	        s2.tieuDe = "Cấu trúc dữ liệu";
+	        s2.tacGia = "Trần Văn B";
+	        s2.gia = 150000;
+	        s2.soTrang = 400;
+
+	        	Sach s3 = new Sach();
+	        s3.tieuDe = "OOP Java";
+	        s3.tacGia = "Lê Văn C";
+	        s3.gia = 180000;
+	        s3.soTrang = 500;
+
+	        s1.hienThiThongTin();
+	        s2.hienThiThongTin();
+	        s3.hienThiThongTin();
+}
 }
