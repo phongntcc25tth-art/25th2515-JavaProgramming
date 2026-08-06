660be321d6c4af04da522cd1d6126d403801c7af
phongntcc25tth-art <phong.nt.cc25tth@ntu.edu.vn>
2026-08-06 19:37:17 +0700

    Create Sach.java

---
+++ baitaplab4_3thuchanh/src/baitaplab4_3thuchanh/Sach.java
@@ -0,0 +1,18 @@
+package baitaplab4_3thuchanh;
+
+public class Sach {
+
+	public String tieuDe;
+	public String tacGia;
+	public int gia;
+	public int soTrang;
+	public void hienThiThongTin() {
+		// TODO Auto-generated method stub
+		 System.out.println("Tiêu đề: " + tieuDe);
+	        System.out.println("Tác giả: " + tacGia);
+	        System.out.println("Giá: " + gia);
+	        System.out.println("Số trang: " + soTrang);
+	        System.out.println("--------------------");
+	}
+
+}
