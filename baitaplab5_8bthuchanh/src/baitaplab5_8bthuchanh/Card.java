5c1af8491ec219cbe366db188e274816f15b9117
phongntcc25tth-art <phong.nt.cc25tth@ntu.edu.vn>
2026-08-06 22:26:18 +0700

    baitaplab5_8bthuchanh
    Trò chơi Memory Game (đơn giản)

---
+++ baitaplab5_8bthuchanh/src/baitaplab5_8bthuchanh/Card.java
@@ -0,0 +1,11 @@
+package baitaplab5_8bthuchanh;
+
+public class Card {
+	 int giaTri;
+	    boolean daLat;
+
+	    Card(int giaTri) {
+	        this.giaTri = giaTri;
+	        this.daLat = false;
+	    }	
+}
