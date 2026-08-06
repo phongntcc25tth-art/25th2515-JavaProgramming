0b2dc1fcfae9fdccaee7db28d7866c0c776d0f7b
phongntcc25tth-art <phong.nt.cc25tth@ntu.edu.vn>
2026-08-06 19:41:22 +0700

    baitaplab4_c6thuchanh
    Thử thách sáng tạo

---
+++ baitaplab4_c6thuchanh/src/baitaplab4_c6thuchanh/ConMeo.java
@@ -0,0 +1,20 @@
+package baitaplab4_c6thuchanh;
+
+public class ConMeo {
+	 String ten;
+	    int tuoi;
+	    String mauLong;
+	    double canNang;
+
+	    void keu() {
+	        System.out.println(ten + ": Meo meo!");
+	    }
+
+	    void an(String thucAn) {
+	        System.out.println(ten + " đang ăn " + thucAn);
+	    }
+
+	    void ngu() {
+	        System.out.println(ten + " đang ngủ.");
+}
+}
---
+++ baitaplab4_c6thuchanh/src/baitaplab4_c6thuchanh/MainConMeo.java
@@ -0,0 +1,19 @@
+package baitaplab4_c6thuchanh;
+
+public class MainConMeo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 ConMeo meo = new ConMeo();
+
+	        meo.ten = "Tom";
+	        meo.tuoi = 2;
+	        meo.mauLong = "Trắng";
+	        meo.canNang = 3.5;
+
+	        meo.keu();
+	        meo.an("cá");
+	        meo.ngu();
+	}
+
+}
