427e8f5de97d7021e21a30425ebb8e9a211931d7
phongntcc25tth-art <phong.nt.cc25tth@ntu.edu.vn>
2026-08-06 19:20:55 +0700

    baitaplab4_5thuchanh

---
+++ baitaplab4_5thuchanh/src/baitaplab4_5thuchanh/TaiKhoanNganHang.java
@@ -0,0 +1,22 @@
+package baitaplab4_5thuchanh;
+
+public class TaiKhoanNganHang {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public String soTaiKhoan;
+	public String tenChuTaiKhoan;
+	public int soDu;
+	public void hienThiSoDu() {
+		// TODO Auto-generated method stub
+		
+	}
+	public void chuyenTien(TaiKhoanNganHang tk2, int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ baitaplab4_5thuchanh/src/baitaplab4_5thuchanh/taikhoanganhang.java
@@ -0,0 +1,41 @@
+package baitaplab4_5thuchanh;
+public class taikhoanganhang {
+	public class TaiKhoanNganHang {
+
+	    String soTaiKhoan;
+	    String tenChuTaiKhoan;
+	    double soDu;
+	    // Gửi tiền
+	    void guiTien(double soTien) {
+	        soDu += soTien;
+	        System.out.println("Đã gửi " + soTien + " VNĐ");
+	    }
+	    
+	    // Rút tiền
+	    boolean rutTien(double soTien) {
+	        if (soTien <= soDu) {
+	            soDu -= soTien;
+	            return true;
+	        }else {
+	            System.out.println("Không đủ số dư!");
+	        return false;
+	        }
+	    }
+	    // Chuyển tiền
+	    void chuyenTien(TaiKhoanNganHang taiKhoanKhac, double soTien) {
+	        if (rutTien(soTien)) {
+	            taiKhoanKhac.guiTien(soTien);
+	            System.out.println("Chuyển tiền thành công.");
+	        }
+	    }
+	    // Hiển thị số dư
+	    void hienThiSoDu() {
+	    	 System.out.println("Số tài khoản: " + soTaiKhoan);
+	         System.out.println("Chủ tài khoản: " + tenChuTaiKhoan);
+	        System.out.println("Số dư: " + soDu);
+	        System.out.println("Số dư: " + soDu + " VNĐ");
+	        System.out.println("----------------------");
+	    }
+	    }
+	}
+
