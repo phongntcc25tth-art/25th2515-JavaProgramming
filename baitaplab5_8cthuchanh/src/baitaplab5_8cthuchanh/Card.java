b321a43b7f5deacf50ee93a54ba47fd365de481e
phongntcc25tth-art <phong.nt.cc25tth@ntu.edu.vn>
2026-08-06 22:34:44 +0700

    baitaplab5_8cthuchanh

---
+++ baitaplab5_8cthuchanh/src/baitaplab5_8cthuchanh/Card.java
@@ -0,0 +1,11 @@
+package baitaplab5_8cthuchanh;
+
+public class Card {
+	 int giaTri;
+	    boolean daLat;
+
+	    Card(int giaTri) {
+	        this.giaTri = giaTri;
+	        this.daLat = false;
+	    }	
+}
